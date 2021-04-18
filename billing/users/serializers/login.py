200c0adf47433ed6b890a677e1dd1b80eae79ac1
Denis Spiridonov <d.spiridonov@cs.money>
2021-04-18 17:08:41 +0300

    Удалено неиспользуемое поле при создании токена пользователя

--- billing/users/serializers/login.py
+++ billing/users/serializers/login.py
@@ -3,12 +3,3 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class UserTokenObtainPairSerializer(TokenObtainPairSerializer):
     """Сериализатор для получения токена пользователя."""
-
-    @classmethod
-    def get_token(cls, user):
-        """Переопределение полями."""
-        token = super().get_token(user)
-
-        # добавляем кастомные поля
-        token['email'] = user.email
-        return token
